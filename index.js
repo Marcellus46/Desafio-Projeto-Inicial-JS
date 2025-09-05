ce2c8a4da1362b45277703af8434ed8da1bea178
Marcellus46 <marcelobisanchet@gmail.com>
2025-09-05 12:19:56 -0300

    remover linhas em branco do arquivo index.js

--- index.js
+++ index.js
@@ -1,6 +1,41 @@
 console.log("Olá, mundo!");
 
-console.log("Olá, testando"); 
+// Classificador de Nível de Herói.
+
+let NomeDoHeroi = "Gandalf";
+let Xp = "10000";
+let Nivel = "Ascendente";
+
+for (let = 0; i < NomeDoHeroi.length; i++) {
+    let Nome = heroi[i].nome;
+    let Xp = heroi[i].xp;
+    let Nivel = "9000;"
+
+if (Xp >= 1000) {
+    Nivel = "Ferro"
+} else if (Xp >= 1001 && xp <= 2000) {
+    Nivel = "Bronze";
+} else if (Xp >= 2001 && xp <= 5000) {
+    Nivel = "Prata";
+} else if (Xp >= 5001 && xp <= 7000) {
+    Nivel = "Ouro";
+} else if (Xp >= 7001 && xp <= 8000) {
+    Nivel = "Platina";
+} else if (Xp >= 8001 && xp <= 9000) {
+    Nivel = "Diamante";
+} else if (Xp >= 9001 && xp <= 10000) {
+    Nivel = "Ascendente";
+} else if (Xp >= 10001 && xp <= 20000) {
+    Nivel = "Imortal";
+} else (Xp >= 20001 && xp <= 30000) {
+    Nivel = "Radiante";
+}
+
+console.log("O heroi Gandalf está no nível de 10000 XP, portanto seu nível é Ascendente.");
+
+
+
+
 
 
 
