9cef2c5ca59f2e26a7e5fd97f907fe521c714d4a
Marcellus46 <marcelobisanchet@gmail.com>
2025-09-05 14:17:07 -0300

    "aleração 3"

--- index.js
+++ index.js
@@ -9,28 +9,27 @@ let Nivel = "Ascendente";
 for (let = 0; i < NomeDoHeroi.length; i++) {
     let Nome = heroi[i].nome;
     let Xp = heroi[i].xp;
-    let Nivel = "10000;"
+    let Nivel = "Ascendente;"
 
 if (Xp >= 1000) {
     Nivel = "Ferro"
-} else if (Xp >= 1001 && xp <= 2000) {
+} else if (Xp >= 1001 && Xp <= 2000) {
     Nivel = "Bronze";
-} else if (Xp >= 2001 && xp <= 5000) {
+} else if (Xp >= 2001 && Xp <= 5000) {
     Nivel = "Prata";
-} else if (Xp >= 5001 && xp <= 7000) {
+} else if (Xp >= 5001 && Xp <= 7000) {
     Nivel = "Ouro";
-} else if (Xp >= 7001 && xp <= 8000) {
+} else if (Xp >= 7001 && Xp <= 8000) {
     Nivel = "Platina";
-} else if (Xp >= 8001 && xp <= 9000) {
+} else if (Xp >= 8001 && Xp <= 9000) {
     Nivel = "Diamante";
-} else if (Xp >= 9001 && xp <= 10000) {
+} else if (Xp >= 9001 && Xp <= 10000) {
     Nivel = "Ascendente";
-} else if (Xp >= 10001 && xp <= 20000) {
+} else if (Xp >= 10001 && Xp <= 20000) {
     Nivel = "Imortal";
-} else if (Xp >= 20001 && xp <= 30000) {
+} else if (Xp >= 20001 && Xp <= 30000) {
     Nivel = "Radiante";
 }
-
 console.log("O heroi Gandalf está no nível de 10000 XP, portanto seu nível é Ascendente.");
 
 
